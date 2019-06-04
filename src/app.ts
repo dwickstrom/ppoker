cee9a8291ee6e4453d9269a76daf0ae66ccecd59
David Wickström <davidwickstrom@gmail.com>
2019-06-04 21:45:55 +0200

    Adjust spacing

--- src/app.ts
+++ src/app.ts
@@ -4,37 +4,35 @@ import { Socket } from 'socket.io';
 import { Connections } from './connection';
 
 export interface AppState {
-  games: Games,
-  connections: Connections,
+    games: Games,
+    connections: Connections,
 }
 
 export interface App {
-  state: AppState[],
-  handleEvent: ((socket: Socket) => (event: handle._Event) => AppState[]),
-  setState: ((state: AppState[]) => void),
-  getState: (() => AppState[]),
+    state: AppState[],
+    handleEvent: ((socket: Socket) => (event: handle._Event) => AppState[]),
+    setState: ((state: AppState[]) => void),
+    getState: (() => AppState[]),
 }
 
 const eventHandler =
-  (socket: Socket,appState: AppState[], event: handle._Event): AppState[] =>
-      event.label === 'ClientConnected'      ? handle.clientConnected(event, socket)(appState)
-    : event.label === 'ClientDisconnected'   ? handle.clientDisconnected(event, socket)(appState)    
-    : event.label === 'PlayerJoinedGame'     ? handle.playerJoinedGame(event, socket)(appState)
-    : event.label === 'PlayerVoted'          ? handle.playerVoted(event, socket)(appState)
-    : /* otherwise */                          handle.theUnknown(event, socket)(appState)
+    (socket: Socket,appState: AppState[], event: handle._Event): AppState[] =>
+          event.label === 'ClientConnected'      ? handle.clientConnected(event, socket)(appState)
+        : event.label === 'ClientDisconnected'   ? handle.clientDisconnected(event, socket)(appState)    
+        : event.label === 'PlayerJoinedGame'     ? handle.playerJoinedGame(event, socket)(appState)
+        : event.label === 'PlayerVoted'          ? handle.playerVoted(event, socket)(appState)
+        : /* otherwise */                          handle.theUnknown(event, socket)(appState)
 
 export const initApp = (initialState: AppState) => {
-  const app: App = {
-    state: [initialState],
-    handleEvent: (socket: Socket) => (event: handle._Event): AppState[] => {
-      let nextState = eventHandler(socket, app.state, event)
-      app.setState(nextState)
-  
-      return app.state
-    },
-    setState: (next: AppState[]) => { app.state = next },
-    getState: () => app.state,
-  }
-
-  return app
+    const app: App = {
+        state: [initialState],
+        handleEvent: (socket: Socket) => (event: handle._Event): AppState[] => {
+            let nextState = eventHandler(socket, app.state, event)
+            app.setState(nextState)    
+            return app.state
+        },
+        setState: (next: AppState[]) => { app.state = next },
+        getState: () => app.state,
+    }
+    return app
 }
